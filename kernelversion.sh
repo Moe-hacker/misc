400fd8f4433e4d963bc5ba3485736b31473d8b98
Hacker-moe <shichengniu2@gmail.com>
2022-12-06 17:03:17 +0800

    继续termux-container重构喵～

--- kernelversion.sh
+++
@@ -1,8 +0,0 @@
-#!/data/data/com.termux/files/usr/bin/bash
-kernelVersion="$(uname -r)"
-kernelMajor="${kernelVersion%%.*}"
-kernelMinor="${kernelVersion#$kernelMajor.}"
-kernelMinor="${kernelMinor%%.*}"
-kernelMinor2="${kernelVersion#*$kernelMinor.}"
-kernelMinor2="${kernelMinor2%%-*}"
-echo "Kernel version: $kernelMajor.$kernelMinor.$kernelMinor2"
